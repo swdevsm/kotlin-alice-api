d62c36bf6dd426392a03196633c0231d20c75d76
swdevsm <13076163+swdevsm@users.noreply.github.com>
2025-11-15 16:08:34 +0300

    add publish action, correct gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,20 @@ plugins {
   alias(libs.plugins.publisher)
 }
 
+fun getEnv(varName: String): String? = System.getenv(varName)
+fun getProperty(propName: String): String? = project.findProperty(propName) as? String
+fun getEnvOrProperty(envVar: String, propName: String = envVar, defaultValue: String? = null): String? {
+  return getEnv(envVar) ?: getProperty(propName) ?: defaultValue
+}
+
+fun requireEnv(envVar: String, propName: String = envVar): String {
+  return getEnvOrProperty(envVar, propName)
+    ?: throw GradleException("Required environment variable or property '$envVar' not found")
+}
+
 allprojects {
   group = "ru.swdevsm"
-  version = "0.0.1"
+  version = getEnvOrProperty("LIBRARY_VERSION", "library.version") ?: "0.0.1-SNAPSHOT"
 
   repositories {
     mavenCentral()
@@ -42,6 +53,12 @@ kotlinPublications {
   defaultArtifactIdPrefix.set("kotlin-alice-api")
   // Set to false if you want to publish empty Javadoc JARs. Maven Central is OK with it
   fairDokkaJars.set(false)
+
+  signingCredentials(
+    key = requireEnv("SIGNING_KEY_ID", "key.id"),
+    privateKey = requireEnv("SIGNING_KEY_PRIVATE", "key.private"),
+    keyPassphrase = requireEnv("SIGNING_KEY_PASSPHRASE", "key.passphrase"),
+  )
 
   //todo: add signingCredentials/sonatypeSettings/pom if need it
 
